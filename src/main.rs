26f43325403aae2da63ac363026f8254ef20f909
Tim <63202655+tmay-sarsaparilla@users.noreply.github.com>
2023-08-14 19:28:00 +0100

    Add: support for partial game files (#10)

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,10 @@ use fenrs::{execute_moves, parse};
 use std::fs;
 
 fn main() {
-    let file = fs::read_to_string("./resources/test/acceptance/chess_com_games_2023-08-13.pgn").unwrap();
-    let pgns = parse(&file);
-    match pgns {
-        Err(error) => {
-            let message = error.message();
-            let length = message.len();
-            println!(
-                "{:?}, {:?}",
-                message.get(0..500),
-                message.get(length - 100..length)
-            )
-        }
-        Ok(_) => {}
+    let file =
+        fs::read_to_string("./resources/test/acceptance/chess_com_games_2023-08-13.pgn").unwrap();
+    let pgns = parse(&file).unwrap();
+    for pgn in pgns.iter() {
+        let _boards = execute_moves(pgn.fen().starting_board(), pgn.ply()).unwrap();
     }
-    // for pgn in pgns.iter() {
-    //     let _boards = execute_moves(pgn.fen().starting_board(), pgn.ply()).unwrap();
-    // }
 }
