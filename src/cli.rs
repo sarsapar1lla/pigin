9fc6936d23af1d83ce62c31d12ab01912e71b076
Tim <63202655+sarsapar1lla@users.noreply.github.com>
2025-04-28 21:54:25 +0100

    Update: improve documentation (#14)
    * Update: improve documentation
    
    * Update: rust toolchain version
    
    * Update: rust edition 2024

--- src/cli.rs
+++ src/cli.rs
@@ -1,14 +1,17 @@
 use clap::{Arg, Command};
 
 pub fn pigin() -> Command {
-    Command::new("pgn").arg(
-        Arg::new("file")
-            .short('f')
-            .long("file")
-            .required(true)
-            .num_args(1..)
-            .help("File or files to visualise"),
-    )
+    Command::new("pigin")
+        .author("sarsapar1lla")
+        .about("Chess visualisation TUI")
+        .arg(
+            Arg::new("file")
+                .short('f')
+                .long("file")
+                .required(true)
+                .num_args(1..)
+                .help("File or files to visualise"),
+        )
 }
 
 #[cfg(test)]
@@ -17,20 +20,21 @@ mod tests {
 
     #[test]
     fn returns_err_if_no_file_provided() {
-        let matches = pigin().try_get_matches_from(&["pgn"]);
+        let matches = pigin().try_get_matches_from(&["pigin"]);
         assert!(matches.is_err())
     }
 
     #[test]
     fn parses_file() {
-        let matches = pigin().get_matches_from(&["pgn", "--file", "example.pgn"]);
+        let matches = pigin().get_matches_from(&["pigin", "--file", "example.pgn"]);
         let files: Vec<_> = matches.get_many::<String>("file").unwrap().collect();
         assert_eq!(files, vec!["example.pgn"])
     }
 
     #[test]
     fn parses_multiple_files() {
-        let matches = pigin().get_matches_from(&["pgn", "--file", "example1.pgn", "example2.pgn"]);
+        let matches =
+            pigin().get_matches_from(&["pigin", "--file", "example1.pgn", "example2.pgn"]);
         let files: Vec<_> = matches.get_many::<String>("file").unwrap().collect();
         assert_eq!(files, vec!["example1.pgn", "example2.pgn"])
     }
